de3095351045c12e8e5962b0ccfd7e54b5847473
Val <135660199+SkullDrg@users.noreply.github.com>
2023-06-05 17:17:28 -0300

    Delete EXCERCISE LIST directory

--- EXCERCISE LIST/Data input and output/Question1.py
+++
@@ -1,6 +0,0 @@
-#                                             QUESTIONS DATABASE
-#                                           DATA INPUT AND OUTPUT
-#                 https://docs.google.com/document/d/1nD_SMgHm3IbEVLh_uYNODUSUI5lmi92p7zUkVD-x_yM/edit#
-
-#1° Question: Print "Hello World" 
-print ("Hello World")
--- EXCERCISE LIST/Data input and output/Question3.py
+++
@@ -1,8 +0,0 @@
-#                                             QUESTIONS DATABASE
-#                                           DATA INPUT AND OUTPUT
-#                 https://docs.google.com/document/d/1nD_SMgHm3IbEVLh_uYNODUSUI5lmi92p7zUkVD-x_yM/edit#
-
-#3° Question: Asks for the Username and print a personalized message
-Username = input("Please, what's your name? ")
-
-print (f"Welcome to the world of programming {Username}")
